f04b8bdb4ac076759899b921e5fe99aea9af80be
ONLIX <67185896+nsde@users.noreply.github.com>
2022-05-04 10:09:14 +0200

    Update Anwendung.java

--- Anwendung.java
+++ Anwendung.java
@@ -19,12 +19,12 @@ public class Anwendung
         meinFenster = new Fenster(400, 400);
         meinFenster.setzeTitel("Schmuck");
         
-        kugel1 = new Kugel();
+        kugel1 = new Kugel(100, 200);
         kugel1.setzeRadius(pRadius);
         kugel1.setzeFadenlaenge(pFadenlaenge);
         kugel1.zeige();
         
-        kugel2 = new Kugel();
+        kugel2 = new Kugel(250, 200);
         kugel2.setzeRadius(pRadius);
         kugel2.setzeFadenlaenge(pFadenlaenge);
         kugel2.zeige();
